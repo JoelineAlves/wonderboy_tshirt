eb0932be8a6d86048f960a5331c40363d6ca0b7a
Joeline Alves <joelinealves08@gmail.com>
2024-11-22 23:38:05 +0000

    setting the database

--- wonderboy_tshirt/settings.py
+++ wonderboy_tshirt/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 import os
+import dj_database_url 
 
 from pathlib import Path
 
@@ -125,15 +126,12 @@ LOGIN_REDIRECT_URL = '/'
 WSGI_APPLICATION = 'wonderboy_tshirt.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+     'default': {
+         'ENGINE': 'django.db.backends.sqlite3',
+         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+     }
+ }
 
 
 # Password validation
