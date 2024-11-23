7ed3f6a544a1aaf1ec4b412114d275a0d85cd8eb
Joeline Alves <joelinealves08@gmail.com>
2024-11-23 15:14:00 +0000

    setting new secret_key

--- wonderboy_tshirt/settings.py
+++ wonderboy_tshirt/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-p*gkx!ihepw+_(3xfukrl(l92338!kl_r*hvfy)@xqbz@(168q'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['wonderboytshirt.herokuapp.com', 'localhost:8000', 'wonderboytshirt-90045f83868e.herokuapp.com']
 
