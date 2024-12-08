034dc4ca8f30b4cfa1c52fe04ffaff8b36a16abd
Joeline Alves <joelinealves08@gmail.com>
2024-12-08 08:59:15 +0000

    fixing the newsletter urls part2

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.newsletter, name='newsletter'),  # Sem o 'newsletter/' duplicado
     path('subscribe/', views.subscribe_to_newsletter, name='subscribe_to_newsletter'),
+    path('newsletter/', views.newsletter, name='newsletter'),
     path('create-newsletter/', views.create_newsletter, name='create_newsletter'),
     path('edit-newsletter/<int:id>/', views.edit_newsletter, name='edit_newsletter'),
     path('delete-newsletter/<int:id>/', views.delete_newsletter, name='delete_newsletter'),
