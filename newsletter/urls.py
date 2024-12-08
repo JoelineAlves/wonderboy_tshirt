dc90758beb525173df0657b873180a8c939e4368
Joeline Alves <joelinealves08@gmail.com>
2024-12-08 09:44:07 +0000

    setting the urls on newsletter urls.py

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('subscribe/', views.subscribe_to_newsletter, name='subscribe_to_newsletter'),
-    path('newsletter/', views.newsletter, name='newsletter'),
+    path('', views.newsletter, name='newsletter'),
     path('create-newsletter/', views.create_newsletter, name='create_newsletter'),
     path('edit-newsletter/<int:id>/', views.edit_newsletter, name='edit_newsletter'),
     path('delete-newsletter/<int:id>/', views.delete_newsletter, name='delete_newsletter'),
