c1d719d0e9b49051a44e04d4cf5c27501a9cc5e9
Joeline Alves <joelinealves08@gmail.com>
2024-12-13 17:31:28 +0000

    change the main-nav

--- home/models.py
+++ home/models.py
@@ -1,13 +1,5 @@
-# home/models.py
-
 from django.db import models
-from django.contrib.auth.models import User
 
-class SubscribeToNewsletter(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='subscription')
-    email = models.EmailField(unique=True)
-    subscribed_at = models.DateTimeField(auto_now_add=True)
+# Create your models here.
 
-    def __str__(self):
-        return self.email
 
--- home/views.py
+++ home/views.py
@@ -1,15 +1,7 @@
 from django.shortcuts import render
-from .models import SubscribeToNewsletter
 
-def index(request):
-    """ A view to return the index page """
-    
-    # Verificar se o usuário está autenticado e inscrito na newsletter
-    if request.user.is_authenticated:
-        subscribed_user = SubscribeToNewsletter.objects.filter(user=request.user).exists()
-    else:
-        subscribed_user = False  # Se não estiver autenticado, assume-se que não está inscrito
 
-    return render(request, 'home/index.html', {
-        'subscribed_user': subscribed_user,  # Passa a variável para o template
-    })
+# Create your views here.
+def home(request):
+    """ Home view """
+    return render(request, 'home/home.html')
