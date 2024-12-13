e612bf7fb91e3613df84527a6c70895e4d852170
Joeline Alves <joelinealves08@gmail.com>
2024-12-13 14:59:53 +0000

    import to home models.py

--- home/models.py
+++ home/models.py
@@ -1,3 +1,13 @@
-from django.db import models
+# home/models.py
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+
+class SubscribeToNewsletter(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='subscription')
+    email = models.EmailField(unique=True)
+    subscribed_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.email
+
