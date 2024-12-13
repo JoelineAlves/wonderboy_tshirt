b2603e57a518ae564c2c20b0a2fffc707d79781d
Joeline Alves <joelinealves08@gmail.com>
2024-12-13 14:51:46 +0000

    fixed newsletter tab display for subscribed users

--- home/views.py
+++ home/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import SubscribeToNewsletter
 
 def index(request):
     """ A view to return the index page """
+    
+    # Verificar se o usuário está autenticado e inscrito na newsletter
+    if request.user.is_authenticated:
+        subscribed_user = SubscribeToNewsletter.objects.filter(user=request.user).exists()
+    else:
+        subscribed_user = False  # Se não estiver autenticado, assume-se que não está inscrito
 
-    return render(request, 'home/index.html')
+    return render(request, 'home/index.html', {
+        'subscribed_user': subscribed_user,  # Passa a variável para o template
+    })
