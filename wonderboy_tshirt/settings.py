a3b47e695f692d97dad3e89d5c52f8e1dbcb8cf0
Joeline Alves <joelinealves08@gmail.com>
2025-01-02 10:14:32 +0000

    update add_review template

--- wonderboy_tshirt/settings.py
+++ wonderboy_tshirt/settings.py
@@ -194,7 +194,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = 'tmp_static_files'
 
 
 
