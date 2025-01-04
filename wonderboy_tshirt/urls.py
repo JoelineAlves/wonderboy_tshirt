16ea04f2d8ac964d79daf6d8cd82943759cbfa36
Joeline Alves <joelinealves08@gmail.com>
2025-01-04 11:03:25 +0000

    add view product

--- wonderboy_tshirt/urls.py
+++ wonderboy_tshirt/urls.py
@@ -28,4 +28,6 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('reviews/', include('reviews.urls')),
     path('subscribe/', include('subscribe.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
