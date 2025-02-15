67aba9595b961f456ee9ce06b5ea1fce99c921b2
JoelineAlves <joelinealves08@gmail.com>
2025-02-15 19:44:41 +0000

    make change ons views.py and quantity-form for bag

--- bag/views.py
+++ bag/views.py
@@ -3,8 +3,11 @@ from django.contrib import messages
 
 from products.models import Product
 
+# Create your views here.
+
 def view_bag(request):
     """ A view that renders the bag contents page """
+
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
@@ -13,22 +16,24 @@ def add_to_bag(request, item_id):
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = request.POST.get('product_size', None)  # Garantindo que 'size' sempre tenha um valor
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
     if size:
-        if item_id in bag:
-            if size in bag[item_id].get('items_by_size', {}):
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_size'].keys():
                 bag[item_id]['items_by_size'][size] += quantity
                 messages.success(request, f'Updated size {size.upper()} {product.name} quantity to {bag[item_id]["items_by_size"][size]}')
             else:
-                bag[item_id].setdefault('items_by_size', {})[size] = quantity
+                bag[item_id]['items_by_size'][size] = quantity
                 messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
         else:
             bag[item_id] = {'items_by_size': {size: quantity}}
             messages.success(request, f'Added size {size.upper()} {product.name} to your bag')
     else:
-        if item_id in bag:
+        if item_id in list(bag.keys()):
             bag[item_id] += quantity
             messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
         else:
@@ -38,12 +43,15 @@ def add_to_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(redirect_url)
     
+
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
 
     product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
-    size = request.POST.get('product_size', None)
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
 
     if size:
@@ -66,12 +74,15 @@ def adjust_bag(request, item_id):
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
 
+
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
 
     try:
         product = get_object_or_404(Product, pk=item_id)
-        size = request.POST.get('product_size', None)
+        size = None
+        if 'product_size' in request.POST:
+            size = request.POST['product_size']
         bag = request.session.get('bag', {})
 
         if size:
